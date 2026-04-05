a0faf8cd384a625d8585e2b8bf3e4ed4c512e634
tawfiq-fa <tawfiq.fa11@gmail.com>
2026-04-05 14:32:58 +0100

    Final: Premium README and Keycloak Fix

--- frontend/atelier/src/auth/keycloack.js
+++ frontend/atelier/src/auth/keycloack.js
@@ -1,11 +1,10 @@
-import Keycloak from "keycloak-js";
+import Keycloak from "keycloak-js"; // هاد السطر ضروري مايتمسحش!
 
 // src/auth/keycloack.js
 const keycloak = new Keycloak({
   url: "http://localhost:8080",
   realm: "bookstore-realm",
-  clientId: "bookstore-frontend",
+  clientId: "bookstore-frontend", 
 });
-
 
 export default keycloak;
